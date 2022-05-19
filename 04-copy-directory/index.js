7bf1e9625119b1540ba5b48b752dda467694b6a1
ScaronTr <forever2652453@gmail.com>
2022-05-19 20:09:48 +0300

    feat: complete 4th subtask

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
 
+copyDir();
+
+async function copyDir() {
+   fs.readdir(__dirname, (err, files) => {
+      if (err) return console.log(err);
+
+      if (files.includes('files-copy')) {
+         fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+            if (err)
+               return console.log(err);
+            files.forEach((item) => {
+               fs.unlink(path.join(__dirname, 'files-copy', item), (err) => err ? console.log(err) : null);
+            });
+         });
+      } else {
+         fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => err ? console.log(err) : null);
+      }
+
+      fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+         if (err) return console.log(err);
+   
+         files.forEach((item) => {
+            fs.copyFile(path.join(__dirname, 'files', item), path.join(__dirname, 'files-copy', item), (err) => err ? console.log(err) : null);
+         });
+      });
+   });
+}
