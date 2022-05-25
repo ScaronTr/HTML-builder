b78985c682ced36eac4d3c0976f63d55cba49e7f
ScaronTr <forever2652453@gmail.com>
2022-05-25 14:55:00 +0300

    fix: edit code

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true } , (err, files) => {
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true } , async (err, files) => {
    if (err) return console.log(err);
 
-   const writableStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+   let dir = await fsPromises.readdir(path.join(__dirname, 'project-dist'));
+   if (dir.includes('bundle.css')) {
+      await fsPromises.unlink(path.join(__dirname, 'project-dist', 'bundle.css'));
+   }
+   await fsPromises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '');
 
-   files.forEach((item) => {
+   files.forEach(async (item) => {
       if (item.isFile() && path.parse(item.name).ext === '.css') {
-         let readableStream = fs.createReadStream(path.join(__dirname, 'styles', item.name));
-         readableStream.pipe(writableStream);
+         let data = await fsPromises.readFile(path.join(__dirname, 'styles', item.name), 'utf-8');
+         fsPromises.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data);
       }
    });
 })
